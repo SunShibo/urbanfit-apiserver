d9d94497988912c442536f3cb30c468d4a50a2a2
shishaonan <13718725223@163.com>
2018-04-03 17:33:49 +0800

    定时器

---
+++ src/main/java/com/urbanfit/apiserver/timer/Timer.java
@@ -0,0 +1,11 @@
+package com.urbanfit.apiserver.timer;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Administrator on 2018/4/3.
+ */
+@Component
+public class Timer {
+
+}
