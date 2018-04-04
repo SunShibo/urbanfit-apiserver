329f68bcd5081521bb2eba066bba1f3b3be00261
shishaonan <13718725223@163.com>
2018-04-04 13:10:21 +0800

    定时器

---
+++ src/main/java/com/urbanfit/apiserver/web/controller/ClientManage/ClientModuleController.java
@@ -0,0 +1,16 @@
+package com.urbanfit.apiserver.web.controller.ClientManage;
+
+import com.urbanfit.apiserver.web.controller.base.BaseCotroller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by Administrator on 2018/4/4.
+ */
+@Controller
+@RequestMapping("/cmodule")
+public class ClientModuleController extends BaseCotroller{
+
+}
