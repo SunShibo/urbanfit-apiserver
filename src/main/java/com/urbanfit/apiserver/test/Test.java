69ba3f6a93fe647b899a990435eb1b38cb72c089
sunshibo_mac <395831708@qq.com>
2018-04-11 23:42:32 +0800

    ......

--- src/main/java/com/urbanfit/apiserver/test/Test.java
+++ src/main/java/com/urbanfit/apiserver/test/Test.java
@@ -1,10 +1,14 @@
 package com.urbanfit.apiserver.test;
 
+import com.urbanfit.apiserver.util.env.Env;
+
 /**
  * Created by Shibo on 17/1/5.
  */
 public class Test {
 
 
-
+    public static void main(String[] args) {
+        System.out.println(new Env().getProperty("jdbc.password"));
+    }
 }
